f18a0e13da8c580fe3fa774e4806d33cd8b6fc9a
Oscar Loayza <loayza.oscar@gmail.com>
2020-12-29 11:42:07 +0100

    Fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,21 +10,41 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any = {};
   constructor( private http: HttpClient ) {
 
-        // Leer el archivo JSON
-    this.http.get('assets/data/data-pagina.json')
+  this.cargarInfo();
+  this.cargarEquipo();
+
+  }
+
+  private cargarInfo(){
+
+      // Leer el archivo JSON
+      this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
 
         this.cargada= true;
         this.info = resp;
+
+
+      })
+
+  }
+
+  private cargarEquipo(){
+
+      // Leer el archivo JSON
+      this.http.get('https://angular-html-1a146-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe( (resp: any) => {
+
+
+        this.equipo = resp;
         console.log(resp)
 
       })
 
-
-
-
-
   }
+
+
 }
